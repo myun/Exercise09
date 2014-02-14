c8f9523bd5bc5eb43a384f6ce4f01b48ba53b079
Hackbright Student <user@mini-005.(none)>
2014-02-13 18:06:29 -0800

    Completed up to Palindrome, Other functions explode in a firey recursive death.

--- recursion.py
+++ recursion.py
@@ -40,32 +40,57 @@ print sum_list([1, 2])
 
 # Reverse a list without slicing or loops
 def reverse(l):
-    new_list = []
     if len(l) == 1:
-        new_list.append(l[0])
-        return new_list
-    else:
-        new_list.append(l.pop())
-        reverse(l)
-        print new_list
-    return new_list
-
+        return l
+    popped = l.pop()
+    return [popped] + reverse(l)
 
 print reverse([1,2,3,4,5])
+
 # Fibonacci returns the nth fibonacci number. The nth fibonacci number is
 # defined as fib(n) = fib(n-1) + fib(n-2)
 def fibonacci(n):
-    pass
+    #Come back to fill out base case
+    if n == 1 or n == 0:
+        return n
+    else:
+        n = fibonacci(n-1) + fibonacci(n-2)
+        return n
+#it works and I want to eat small children now. Thanks, Fibonacci! :)
+print fibonacci(10)
 
 # Finds the item i in the list l.... RECURSIVELY
 def find(l, i):
-    return None
+    popped = l.pop()
+    if popped == i:
+        return popped
+    elif len(l) == 0:
+        #FLAMES!!!
+        return "Sorry, we have no %r" % i
+    else:
+        return find(l, i)
+
+
+print find([1,2,3,4,5,6,7], 'bananas')
 
 # Determines if a string is a palindrome
 def palindrome(some_string):
-    return False
+#I have no idea how we did this, but IT WORKS!
+    some_list = some_string.split()
+    some_letter = some_list.pop()
+    if len(some_list) == 0:
+        return True
+    elif some_letter == some_list[0]:
+        del some_list[0]
+        palindrome(some_list)
+    else:
+        return False
 
-# Given the width and height of a sheet of paper, and the number of times to fold it, return the final dimensions of the sheet as a tuple. Assume that you always fold in half along the longest edge of the sheet.
+print palindrome("mom")
+
+# Given the width and height of a sheet of paper, and the number of times to fold it, 
+#return the final dimensions of the sheet as a tuple. 
+#Assume that you always fold in half along the longest edge of the sheet.
 def fold_paper(width, height, folds):
     return (0, 0)
 
@@ -73,3 +98,6 @@ def fold_paper(width, height, folds):
 # Print all the numbers from 0 to target
 def count_up(target, n):
     return
+    #look at factorial
+    # use another variable
+    #nap. 
